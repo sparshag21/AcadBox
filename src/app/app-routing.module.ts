4b053d4de1c7769904114ee787f76e8a866c0700
Sparsh Agarwal <sparsh21agarwal@gmail.com>
2019-07-01 19:19:27 +0530

    add routerlink and edit dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginpageComponent },
+  { path: 'about', component: AboutComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,10 +24,11 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
 import { RegisterComponent } from './register/register.component';
 import { ShowFilesComponent } from './show-files/show-files.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatTableFilterModule } from 'mat-table-filter';
 import { LoginSinglePageComponent } from './login-single-page/login-single-page.component';
 import { AlertBoxComponent } from './alert-box/alert-box.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
@@ -42,7 +43,8 @@ import { AlertBoxComponent } from './alert-box/alert-box.component';
     ShowFilesComponent,
     DashboardComponent,
     LoginSinglePageComponent,
-    AlertBoxComponent
+    AlertBoxComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
