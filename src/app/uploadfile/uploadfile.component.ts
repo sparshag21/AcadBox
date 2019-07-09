f213c7f1d556aa46571094c0499436c1c90c797e
yjgarg <yuvraj.dakshana18@gmail.com>
2019-07-10 01:33:07 +0530

    UploadFile component temporarily completed

--- src/app/uploadfile/uploadfile.component.ts
+++ src/app/uploadfile/uploadfile.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AngularFirestoreDocument } from '@angular/fire/firestore';
 import { AngularFirestoreCollection } from '@angular/fire/firestore';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { File } from '../file';
-import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from '../user';
-import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-uploadfile',
   templateUrl: './uploadfile.component.html',
   styleUrls: ['./uploadfile.component.css']
 })
-export class UploadfileComponent implements OnInit {
-  user: User;
+export class UploadfileComponent {
+  myform = FormGroup;
+  userDocument: AngularFirestoreDocument<User>;
+  user: User = {
+    name: '',
+    username: '',
+    roll: '',
+    email: '',
+    courses: [],
+    uid: ''
+  };
+  uid: string = '';
 
-  userDocument: Observable<User>;
   doccollection: AngularFirestoreCollection<File>;
   docs: AngularFirestoreDocument<File>;
 
@@ -24,12 +36,34 @@ export class UploadfileComponent implements OnInit {
   drivelink: string;
   fileName: string;
 
-  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) { }
+  constructor(private breakpointObserver: BreakpointObserver, private afs: AngularFirestore, private afAuth: AngularFireAuth, private router: Router) {
+    afAuth.auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.uid = user.uid;
+      }
+      else {
+        this.uid = 'guest';
+      }
+      this.fetchUserData();
+    });
+  }
+
+  fetchUserData() {
+    if (this.uid != 'guest') {
+      this.userDocument = this.afs.doc<User>('users/' + this.uid);
+      this.userDocument.valueChanges().subscribe((user) => {
+        this.user = user;
+      })
+    }
+    else {
+      this.user.username = 'Guest';
+    }
+  }
 
   postfile() {
-    this.docs = this.afs.doc<File>('course/' + this.currcourse);
+    this.docs = this.afs.doc<File>('courses/' + this.currcourse);
     this.doccollection = this.docs.collection<File>(this.doctype);
-    let newFile: File = {
+    const newFile: File = {
       link: this.drivelink,
       currcourse: this.currcourse,
       doctype: this.doctype,
@@ -38,17 +72,8 @@ export class UploadfileComponent implements OnInit {
       description: this.fileName,
       rating: 0
     };
+    // console.log("uploading");
     this.doccollection.doc(newFile.uid).set(newFile);
+    window.alert('Uploaded Successfully');
   }
-
-  ngOnInit() {
-    this.afAuth.auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.afs.doc<User>('users' + user.uid).valueChanges().subscribe((user) => {
-          this.user = user;
-        });
-      }
-    });
-  }
-
 }
