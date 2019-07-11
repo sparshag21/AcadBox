3aebcda9e1b14915fce8464408c27b81a673bae3
Yuvraj Garg <yuvraj.dakshana18@gmail.com>
2019-07-11 21:57:49 +0530

    Delete mock-files.ts
    No need of this file

--- src/app/mock-files.ts
+++
@@ -1,7 +0,0 @@
-import { File } from './file';
-
-export const FILES: File[] = [
-    { link: "https://drive.google.com/file/d/10D2JqMsiD-ijaVihae6U8R7J6EwdDXgZ/preview", name: 'Yuvraj Garg', rating: 4, uid: "", uploader : { uid: "", username: "" }}, 
-    { link: "https://drive.google.com/file/d/10D2JqMsiD-ijaVihae6U8R7J6EwdDXgZ/preview", name: 'Shyam', rating: 3, uid: "", uploader : { uid: "", username: "" }},
-    { link: "https://drive.google.com/file/d/10D2JqMsiD-ijaVihae6U8R7J6EwdDXgZ/preview", name: 'Ram', rating: 2, uid: "", uploader : { uid: "", username: "" }},
-];
